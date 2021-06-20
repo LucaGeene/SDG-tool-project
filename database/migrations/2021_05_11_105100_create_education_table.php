404803e4d8abd7d292089c8f94315759d457dba6
Luca Geene <70521173+LucaGeene@users.noreply.github.com>
2021-06-20 10:41:35 +0200

    Update 2021_05_11_105100_create_education_table.php

--- database/migrations/2021_05_11_105100_create_education_table.php
+++ database/migrations/2021_05_11_105100_create_education_table.php
@@ -17,6 +17,7 @@ class CreateEducationTable extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
+            $table->string('url');
             $table->timestamps();
         });
     }
